d5ebecb467020f2ec57a35309d244f60b137c755
Gleison Lemos <gleison04lemos@gmail.com>
2023-10-02 23:31:15 -0300

    feat: service layer

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -5,37 +5,53 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $supports)
+    public function __construct(protected SupportService $service)
+    {
+
+    }
+
+    public function index(Request $request)
     { 
-        $supports = $supports->all(); 
+        $supports = $this->service->getAll($request->filter); 
         
         return view('admin.supports.index', compact('supports'));
     }
-
+    
+    public function show(string $id)
+    {
+        // Support::find($id
+        // Support::where('id', $id)->first();
+        // Support::where('id', '=', $id)->first();
+        if (!$support = $this->service->findOne($id)) {
+            return back();
+        }   
+        
+        return view('admin.supports.show', compact('support'));
+    }
+    
     public function create()
     {
         return view('admin.supports.create');
     }
 
-    public function show(string|int $id)
+    public function store(StoreUpdateSupport $request, Support $support)
     {
-        // Support::find($id
-        // Support::where('id', $id)->first();
-        // Support::where('id', '=', $id)->first();
-        if (!$support = Support::find($id)) {
-            return back();
-        }   
+        $data = $request->validated();
+        $data['status'] = 'a';
 
-        return view('admin.supports.show', compact('support'));
+        $support->create($data);
+        return redirect()->route('supports.index');
     }
 
-    public function edit(Support $support, string|int $id)
+    public function edit(string $id)
     {
-        if (!$support = Support::where('id', $id)->first()) {
+        //if (!$support = Support::where('id', $id)->first()) {
+        if (!$support = $this->service->findOne($id)) {
             return back();
         } 
           
@@ -57,23 +73,11 @@ class SupportController extends Controller
         return redirect()->route('supports.index');
     }
 
-    public function destroy(string|int $id)
+    public function destroy(string $id)
     {
-        if (!$support = Support::find($id)) {
-            return back();
-        }
-
-        $support->delete();
+        $this->service->delete($id);
         
         return redirect()->route('supports.index');
     }
 
-    public function store(StoreUpdateSupport $request, Support $support)
-    {
-        $data = $request->validated();
-        $data['status'] = 'a';
-
-        $support->create($data);
-        return redirect()->route('supports.index');
-    }
 }
