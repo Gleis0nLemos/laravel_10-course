76a660cc4e4a09452f12d4f13d90876f593f0662
Gleison Lemos <gleison04lemos@gmail.com>
2023-10-03 13:20:29 -0300

    feat: starting to repository implement

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -4,22 +4,46 @@ use App\DTOs\{
     CreateSupportDTO,
     UpdateSupportDTO,
 };
+use App\Models\Support;
 use App\Repositories\SupportRepositoryInterface;
 
 class SupportEloquentORM implements SupportRepositoryInterface
-{
+{   
+    public function __construct(
+        protected Support $model
+    ) {}
+
     public function getAll(string $filter=null): array
     {
-        
+        return $this->model
+                    ->where(function ($query) use ($filter) {
+                        if ($filter) {
+                            $query->where("subject", $filter);
+                            $query->orWhere("body", 'like', "%{$filter}%");
+                        }
+                    })
+                    ->all()
+                    ->toArray(); 
     }
+
     public function findOne(string $id): stdClass|null
     {
-        
+        $support = $this->model
+                        ->find($id)
+                        ->toArray();
+
+        if(!$support) {
+            return null;
+        }
+
+        return $support = (object) $support->toArray();
     }
+
     public function delete(string $id): void
     {
-        
+        $this->model->findOrFail($id)->delete();
     }
+    
     public function new(CreateSupportDTO $dto): stdClass
     {
         
