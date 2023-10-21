8b453d5892960e93c14dcf3159ddcd884b3e5a7c
Gleison Lemos <gleison04lemos@gmail.com>
2023-10-21 18:51:09 -0300

    style: start UI/UX aplication

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title') - {{ config('app.name') }}</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+
+    <head>
+        @yield('header')
+    </head>
+    <div class="content">
+        @yield('content')
+    </div>
+    <footer>
+        #defaut footer
+    </footer>
+</body>
+
+</html>
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,32 +1,37 @@
-<h1>Supports</h1>
+@extends('admin.layouts.app')
 
-<a href="{{ route('supports.create') }}">Nova dúvida </a>
-                
+@section('title', 'Supports')
 
-<table>
-    <thead>
-        <th>assunto</th>
-        <th>status</th>
-        <th>descrição</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach ($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ getStatusSupport($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">Ver</a>
-                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@section('header')
+    <h1>Supports</h1>
+@endsection
 
-<x-pagination 
-    :paginator="$supports"
-    :appends="$filters"
-/>
-    
+@section('content')
+    <a href="{{ route('supports.create') }}">Nova dúvida </a>
+
+
+    <table>
+        <thead>
+            <th>assunto</th>
+            <th>status</th>
+            <th>descrição</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @foreach ($supports->items() as $support)
+                <tr>
+                    <td>{{ $support->subject }}</td>
+                    <td>{{ getStatusSupport($support->status) }}</td>
+                    <td>{{ $support->body }}</td>
+                    <td>
+                        <a href="{{ route('supports.show', $support->id) }}">Ver</a>
+                        <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <x-pagination :paginator="$supports" :appends="$filters" />
+
+@endsection
